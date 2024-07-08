5d8ad3b229cab05fe8f61f51558e084094931318
denisKharke <hda@hpc-park.ru>
2024-07-08 17:57:59 +0300

    add router links

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,13 +1,27 @@
 <template>
-  <div>
-    Navigation
-  </div>
+  <nav id="navigation">
+    <div class="wrapper">
+      <RouterLink :to="{}">
+        <img src="@/assets/logo.svg" class="logo">
+      </RouterLink>
+    </div>
+  </nav>
 </template>
 
 <script setup lang="ts">
-
+import { RouterLink } from 'vue-router'
 </script>
 
 <style scoped>
-
+  .wrapper {
+    padding: 2rem;
+    background: black;
+    color: white;
+    font-size: 1.25rem;
+    font-weight: 700;
+  }
+  
+  .logo {
+    height: 40px;
+  }
 </style>
